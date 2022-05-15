16567db83cc6626fc41342b4e27479cab4293398
Gamerssassin <gamersassin@gmail.com>
2022-05-15 18:00:25 +0400

    keybind fix

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -4,7 +4,7 @@ local keymap  = vim.api.nvim_set_keymap
 
 keymap("n", "<C-f>", ":NERDTreeToggle<CR>", opts)
 keymap("n","<A-f>", ":Goyo<CR>", opts)
-keymap("n", "C-h", "<C-w>h", opts)
-keymap("n", "C-j", "<C-w>j", opts)
-keymap("n", "C-k", "<C-w>k", opts)
-keymap("n", "C-l", "<C-w>l", opts)
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
