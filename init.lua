6093d9f0d8b114d2eae464f92345cc3c4ddfbbbf
Gamerssassin <gamersassin@gmail.com>
2022-05-15 16:22:14 +0400

    Changing to lua

---
+++ init.lua
@@ -0,0 +1,18 @@
+require('plugins')
+require('treesitter-config')
+require('keymaps')
+
+vim.cmd('colorscheme nightfox')
+vim.cmd('set wrap')
+vim.cmd('set hidden')
+vim.cmd('noswapfile')
+vim.cmd('set noerrorbells')
+vim.cmd('set smartindent')
+vim.cmd('set nowrap')
+vim.cmd('set incsearch')
+vim.cmd('filetype indent plugin on')
+vim.cmd('set tabstop=4 softtabstop=4')
+vim.cmd('set encoding=utf-8')
+
+
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,15 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'preservim/nerdtree'
+  use 'Xuyuanp/nerdtree-git-plugin'
+  use 'ryanoasis/vim-devicons'
+  use 'farmergreg/vim-lastplace'
+  use 'jiangmiao/auto-pairs'
+  use 'ackyshake/VimCompletesMe'
+  use 'luochen1990/rainbow'
+  use 'EdenEast/nightfox.nvim'
+  use 'junegunn/goyo.vim'
+  use 'tpope/vim-fugitive'
+  use {'nvim-treesitter/nvim-treesitter', run= ":TSUpdate"}
+  use 'neovim/nvim-lspconfig'
+end)
