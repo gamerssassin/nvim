4c7950e6cbb84ef997cc00a6c80cfbe7f7a6984b
Gamerssassin <gamersassin@gmail.com>
2022-05-16 18:07:38 +0400

    lsp setup changes

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -3,7 +3,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = { "c", "lua", "rust", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- List of parsers to ignore installing (for "all")
   ignore_install = { "javascript" },
