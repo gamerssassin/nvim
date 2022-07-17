249539cd03d330ef92c222bbeb455b083804892a
Gamerssassin <gamersassin@gmail.com>
2022-07-18 01:44:21 +0400

    cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,6 +22,12 @@ return require('packer').startup(function()
   use 'machakann/vim-highlightedyank'
   -- use 'tomasr/molokai'
   use 'octol/vim-cpp-enhanced-highlight'
+  use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
+  use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" }) -- buffer completions
+  use({ "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" }) -- path completions
+  use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
+  use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
+  use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
   use {'nvim-treesitter/nvim-treesitter', run= ":TSUpdate"}
   use 'hrsh7th/nvim-compe'
   use 'junegunn/fzf.vim'
